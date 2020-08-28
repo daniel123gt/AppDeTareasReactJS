495675387c63ad8394fa84f8afba2e39b205ca1b
DANIEL <dailgozalez@gmail.com>
2020-08-27 21:56:26 -0500

    Updates

---
+++ precache-manifest.403d44b79299a09039074db88a37eca0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5726e4bb4188ebf05f89dd7d2e8765",
+    "url": "/AppDeTareasReactJS/index.html"
+  },
+  {
+    "revision": "3bdba9c15231910b3687",
+    "url": "/AppDeTareasReactJS/static/css/main.e6175959.chunk.css"
+  },
+  {
+    "revision": "f3f73ef6e7d2507e5a2f",
+    "url": "/AppDeTareasReactJS/static/js/2.ef7382d0.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/AppDeTareasReactJS/static/js/2.ef7382d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdba9c15231910b3687",
+    "url": "/AppDeTareasReactJS/static/js/main.038f1fc6.chunk.js"
+  },
+  {
+    "revision": "a2f3252125909a45c648",
+    "url": "/AppDeTareasReactJS/static/js/runtime-main.0c8cc9ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AppDeTareasReactJS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d993bff95be4f2af75fa790fea98da1",
+    "url": "/AppDeTareasReactJS/static/media/logo.5d993bff.png"
+  }
+]);
